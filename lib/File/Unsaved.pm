e858bdeca560ca7ec9f147f5915619febd828ebd
perlancar <perlancar@gmail.com>
2014-10-09 09:28:33 +0000

    Import of PERLANCAR/File-Unsaved-0.02 from CPAN.
    gitpan-cpan-distribution: File-Unsaved
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PERLANCAR/File-Unsaved-0.02.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/File/Unsaved.pm
+++ lib/File/Unsaved.pm
@@ -1,7 +1,7 @@
 package File::Unsaved;
 
 our $DATE = '2014-10-09'; # DATE
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 use 5.010001;
 use strict;
@@ -19,15 +19,22 @@ $SPEC{check_unsaved_file} = {
     description => <<'_',
 
 This function tries, using some heuristics, to find out if a file is being
-opened and has unsaved modification in an editor. Currently the supported editor
-is Emacs.
+opened and has unsaved modification in an editor. Currently the supported
+editors are: Emacs, joe, vim.
 
 Return false if no unsaved data is detected, or else a hash structure. Hash will
 contain these keys: `editor` (kind of editor).
 
 The heuristics are as folow:
 
-* Emacs: check whether `.#<name>` (symlink) exists.
+* Emacs and joe: check whether `.#<name>` (symlink) exists. Emacs targets the
+  symlink to `<user>@<host>.<PID>:<timestamp>` while joe to
+  `<user>@<host>.<PID>`. Caveat: Unix only.
+
+* vim: check whether `.<name>.swp` exists, newer than file, and its 0x03ef-th
+  byte has the value of `U` (which vim uses to mark the file as unsaved).
+  Caveat: vim can be instructed to put swap file somewhere else or not create
+  swap file at all, so in those cases unsaved data will not be detected.
 
 _
     args => {
@@ -47,13 +54,30 @@ sub check_unsaved_file{
     my $path = $args{path};
     (-f $path) or die "File does not exist or not a regular file";
 
-    # emacs
+    my ($vol, $dir, $file) = File::Spec->splitpath($path);
+
+    # emacs & joe
     {
-        my ($vol, $dir, $file) = File::Spec->splitpath($path);
         my $spath = File::Spec->catpath($vol, $dir, ".#$file");
-        if (-l $spath) {
+        last unless -l $spath;
+        my $target = readlink $spath;
+        if ($target =~ /:\d+$/) {
             return {editor=>'emacs'};
+        } else {
+            return {editor=>'joe'};
         }
+    }
+
+    # vim
+    {
+        my $spath = File::Spec->catpath($vol, $dir, ".$file.swp");
+        last unless -f $spath;
+        last if (-M $spath) > (-M $path); # swap file is older
+        open my($fh), "<", $spath or last;
+        sysseek $fh, 0x03ef, 0 or last;
+        sysread $fh, my($data), 1 or last;
+        $data eq 'U' or last;
+        return {editor => 'vim'};
     }
 
     undef;
@@ -74,7 +98,7 @@ File::Unsaved - Check whether file has unsaved modification in an editor
 
 =head1 VERSION
 
-This document describes version 0.01 of File::Unsaved (from Perl distribution File-Unsaved), released on 2014-10-09.
+This document describes version 0.02 of File::Unsaved (from Perl distribution File-Unsaved), released on 2014-10-09.
 
 =head1 SYNOPSIS
 
@@ -92,8 +116,8 @@ This document describes version 0.01 of File::Unsaved (from Perl distribution Fi
 Check whether file has unsaved modification in an editor.
 
 This function tries, using some heuristics, to find out if a file is being
-opened and has unsaved modification in an editor. Currently the supported editor
-is Emacs.
+opened and has unsaved modification in an editor. Currently the supported
+editors are: Emacs, joe, vim.
 
 Return false if no unsaved data is detected, or else a hash structure. Hash will
 contain these keys: C<editor> (kind of editor).
@@ -102,7 +126,14 @@ The heuristics are as folow:
 
 =over
 
-=item * Emacs: check whether C<< .#E<lt>nameE<gt> >> (symlink) exists.
+=item * Emacs and joe: check whether C<< .#E<lt>nameE<gt> >> (symlink) exists. Emacs targets the
+symlink to C<< E<lt>userE<gt>@E<lt>hostE<gt>.E<lt>PIDE<gt>:E<lt>timestampE<gt> >> while joe to
+C<< E<lt>userE<gt>@E<lt>hostE<gt>.E<lt>PIDE<gt> >>. Caveat: Unix only.
+
+=item * vim: check whether C<< .E<lt>nameE<gt>.swp >> exists, newer than file, and its 0x03ef-th
+byte has the value of C<U> (which vim uses to mark the file as unsaved).
+Caveat: vim can be instructed to put swap file somewhere else or not create
+swap file at all, so in those cases unsaved data will not be detected.
 
 =back
 
